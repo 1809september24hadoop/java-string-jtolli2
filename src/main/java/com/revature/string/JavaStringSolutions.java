08c8df923b4b5b1d082d18ca543dd33f61930350
Jeffrey Tolliver <jtolli2@lsu.edu>
2018-10-31 13:16:17 -0400

    Initial push

---
+++ src/main/java/com/revature/string/JavaStringSolutions.java
@@ -0,0 +1,47 @@
+package com.revature.string;
+
+public class JavaStringSolutions implements JavaString {
+
+	@Override
+	public int parseInteger(String number) throws IllegalArgumentException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String reverse(String string) throws IllegalArgumentException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void time(StringBuilder builder, StringBuffer buffer) throws IllegalArgumentException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean itContains(String string, String contains) throws IllegalArgumentException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public String sort(String string) throws IllegalArgumentException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String delete(String string, char c) throws IllegalArgumentException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String upperLower(String string) throws IllegalArgumentException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
